904a8c896815192d2dfd9cd9036e7461ff807522
beardedpayton <beardedpayton@gmail.com>
2018-02-27 10:26:26 -0500

    looped through stats data on pokemone view

--- src/components/PokemonView.vue
+++ src/components/PokemonView.vue
@@ -6,12 +6,26 @@
           <v-layout row wrap>
           <v-flex xs6>
           <h1>{{pokeData.name}}</h1>
+          <img :src="pokeData.sprites.front_default">
           </v-flex>
           <v-flex xs6>
             <ul>
               <li>Weight: {{pokeData.weight}}</li>
               <li>Height: {{pokeData.height}}</li>
             </ul>
+          </v-flex>
+          <v-flex xs12>
+            <h1>Stats</h1>
+            <hr>
+            <div
+            class="stats"
+            v-for="stat in pokeData.stats"
+            :key="stat.stat.name">
+              <div>
+                <span class="stat-name">{{stat.stat.name}}</span>
+                <span class="stat-number">{{stat.base_stat}}</span>
+              </div>
+            </div>
           </v-flex>
           </v-layout>
         </div>
@@ -39,14 +53,16 @@ export default {
 <style>
 .poke-card {
   border: 3px solid black;
-  height: 530px;
   padding: 25px;
   text-align: center;
 }
-.poke-card h1 {
+h1 {
   text-transform: uppercase;
 }
 ul {
   list-style-type: none;
+}
+.stat-name {
+  text-transform: uppercase;
 }
 </style>
